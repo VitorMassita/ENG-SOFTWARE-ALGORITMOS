2bc9fbe2c46562d568c835698db801b5be100558
Vitor Massita Zucareli <vitor.mz@puccampinas.edu.br>
2026-03-10 09:28:25 -0300

    Aula dia 10/03/2026

--- Exercicios-Em-Sala/aula004-divisao.py
+++ Exercicios-Em-Sala/aula004-divisao.py
@@ -1,7 +1,7 @@
 try:
     x = float(input("Digite o valor de x: "))
     y = (4 * x**2 - 3 * x + 9) / x
-    print(f"O valor de y é: {y}")
+    print(f"O VALOR DE f {x:0.0f} : y {y:0.2f}")
 except ValueError: 
     print("OPS! PORFAVOR DIGITE SOMENTE VALORES NUMERICOS")
 except ZeroDivisionError:
---
+++ Exercicios-Em-Sala/aula004-try-IMC.py
@@ -0,0 +1,18 @@
+try:
+    peso=float(input("DIGITE SEU PESO: "))
+    print("**********************************")
+    altura=float(input("DIGITE SUA ALTURA EM METROS: "))
+    print("**********************************")
+    imc=peso/altura**2
+    if imc < 18.4:
+        print(f"SEU IMC : {imc:0.2f} E ESTA ABAIXO DO PESO")
+    elif (imc >= 18.5) and (imc<=24.9):
+        print((f"SEU IMC : {imc:0.2f} E ESTA NORMAL"))
+    elif (imc >=25) and (imc<= 29.9):
+        print((f"SEU IMC : {imc:0.2f} E ESTA COM SOBREPESO"))
+    else:
+        print("VOCE ESTA COM SOBREPESO")
+except ValueError:
+    print("DIGITE VALORES NUMERICOS")
+except ZeroDivisionError:
+    print("NUNCA DIVIDIR POR 0")
